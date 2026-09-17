73b785fa7bfde6f9204b0f5606b0026207008839
s6904033612123-glitch <s6904033612123@email.kmutnb.ac.th>
2026-09-17 14:10:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
    ========================================================================== */
 
 // Config URLs (Replace with your actual Google Apps Script & Published CSV URLs)
-const APPS_SCRIPT_URL = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
-const CSV_URL = 'YOUR_PUBLISHED_GOOGLE_SHEET_CSV_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz-uAD_ozBHm-pRuUaeUl3Ll2yVPSrAw5ZVRItAvlzoA_JhNH8I9VDfwgmvypAyE53P/exec';
+const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQdEblMsO7DFYMYtIpmf_csGH-ojLcLVYD14aBpEm_hdBe6CIXCnTF0uX2VxMZbP6t1LTr6Dq0e_2Mv/pub?output=csv';
 
 document.addEventListener('DOMContentLoaded', () => {
   initProductPage();
